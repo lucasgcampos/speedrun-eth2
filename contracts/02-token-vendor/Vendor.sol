1d00aa9352fa3cc0ef1b43a9499fcca3c48003b2
Lucas Campos <lucasgcampos.contato@gmail.com>
2024-04-16 23:41:33 -0300

    Fix checkpoint 2

--- contracts/02-token-vendor/Vendor.sol
+++ contracts/02-token-vendor/Vendor.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
 
 contract Vendor is Ownable {
+
   event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
 
   uint256 public constant tokensPerEth = 100;
@@ -15,6 +16,7 @@ contract Vendor is Ownable {
     yourToken = YourToken(tokenAddress);
   }
 
+  // ToDo: create a payable buyTokens() function:
   function buyTokens() payable external {
     uint256 amountOfTokens = msg.value * tokensPerEth;
 
@@ -23,8 +25,9 @@ contract Vendor is Ownable {
     emit BuyTokens(msg.sender, msg.value, amountOfTokens);
   }
 
+  // ToDo: create a withdraw() function that lets the owner withdraw ETH
   function withdraw() external onlyOwner {
-    payable(owner()).call{ value: balance(this) }("");
+    payable(owner()).call{ value: address(this).balance }("");
   }
 
   // ToDo: create a sellTokens(uint256 _amount) function:
--- contracts/02-token-vendor/YourToken.sol
+++ contracts/02-token-vendor/YourToken.sol
@@ -7,6 +7,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract YourToken is ERC20 {
   constructor() ERC20("Gold", "GLD") {
+    _mint(0xd1B939c8853d3EbB276F1F5fEa877ed4B8f01b22, 1000 * 10 ** 18);
     _mint(msg.sender, 1000 * 10 ** 18);
   }
 }
